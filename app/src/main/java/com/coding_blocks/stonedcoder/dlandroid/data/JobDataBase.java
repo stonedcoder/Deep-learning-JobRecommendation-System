cf7ed6d0242e932ab22db5677f7bfb22e70933ce
stonedcoder <vraunaq1213@ghmail.com>
2018-01-25 12:09:43 +0530

    JobDatabase.Class added
    Signed-off-by: stonedcoder <vraunaq1213@ghmail.com>

---
+++ app/src/main/java/com/coding_blocks/stonedcoder/dlandroid/data/JobDataBase.java
@@ -0,0 +1,19 @@
+package com.coding_blocks.stonedcoder.dlandroid.data;
+
+import java.util.LinkedList;
+
+/**
+ * Created by stonedcoder on 25/01/18.
+ */
+
+public class JobDataBase {
+    private static List<LearnableModel> savedModels = new LinkedList<>();
+
+    public static void writeJobs(List<LearnableModel> models) {
+        savedModels.addAll(models);
+    }
+
+    public static List<LearnableModel> readJobs() {
+        return savedModels;
+    }
+}
