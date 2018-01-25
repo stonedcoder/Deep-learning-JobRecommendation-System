f075a684ea6a654ee5e3f140ef6f02c267d91736
stonedcoder <vraunaq1213@ghmail.com>
2018-01-25 10:26:38 +0530

    RxJava , DeepLearning for Java added .
    Signed-off-by: stonedcoder <vraunaq1213@ghmail.com>

--- app/src/main/java/com/coding_blocks/stonedcoder/dlandroid/MainActivity.java
+++ app/src/main/java/com/coding_blocks/stonedcoder/dlandroid/MainActivity.java
@@ -1,9 +1,20 @@
 package com.coding_blocks.stonedcoder.dlandroid;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BinaryClassifier binaryClassifier;
+    private int inputs = 2;
+    private int outputs = 1;
+    RecyclerView recyclerView;
+    private JobAdapter jobAdapter;
+    private Button mockDataButton;
+    private Button learnButton;
+    private Button refreshButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
