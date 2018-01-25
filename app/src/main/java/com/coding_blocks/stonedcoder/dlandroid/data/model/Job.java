3b4112adbe1b46a79b7ddb23db11bc689da6201a
stonedcoder <vraunaq1213@ghmail.com>
2018-01-25 12:34:46 +0530

    Job & BinaryClassifier added , MockData Issues fixed .

---
+++ app/src/main/java/com/coding_blocks/stonedcoder/dlandroid/data/model/Job.java
@@ -0,0 +1,59 @@
+package com.coding_blocks.stonedcoder.dlandroid.data.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by stonedcoder on 25/01/18.
+ */
+
+public class Job implements LearnableModel {
+    private int type;
+    private int salary;
+    private int choice;
+    private int probability;
+
+    public Job(int type, int salary, int choice) {
+        this.type = type;
+        this.salary = salary;
+        this.choice = choice;
+    }
+
+    @Override
+    public void setProbability(int probability) {
+        this.probability = probability;
+    }
+
+    @Override
+    public int[] getFeatures() {
+        return new int[]{type, salary};
+    }
+
+    @Override
+    public void setFeatures(int[] features) {
+        type = features[0];
+        salary = features[1];
+        choice = features[2];
+    }
+
+    @Override
+    public int[] getLabels() {
+        return new int[]{choice};
+    }
+
+    @Override
+    public void setLabels(int[] labels) {
+        this.choice = labels[0];
+    }
+
+    @Override
+    public Map<String, Integer> getValues() {
+        Map<String, Integer> values = new HashMap<>();
+        values.put("type", type);
+        values.put("salary", salary);
+        values.put("choice", choice);
+        values.put("probability", probability);
+        return values;
+    }
+
+}
